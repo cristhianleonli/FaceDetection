35f1756c575d93f8341eace6ab29f6a54af36b77
Cristhian Leon <cris.leonardoleon@gmail.com>
2019-03-26 11:50:49 +0100

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Cristhian Leon on 26.03.19.
+//  Copyright © 2019 Cristhian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
